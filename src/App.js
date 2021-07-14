7e5f7f30328ba3e7797fcbbe68fc4ec40a0acd5f
potchi <richardcorrea2306@gmail.com>
2021-07-14 21:52:19 +0200

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import React, {Component} from 'react';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-        <Navbar dark color="primary">
-            <div className='container'>
-                <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-            </div>
-        </Navbar>
+class App extends Component {
+    constructor(props){
+        super(props);
 
-        <Menu />
-    </div>
-  );
+        this.state = {
+            dishes : DISHES
+        }
+    }
+
+    render(){
+        return (
+          <div>
+              <Navbar dark color="primary">
+                  <div className='container'>
+                      <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
+                  </div>
+              </Navbar>
+        
+              <Menu dishes = {this.state.dishes}/>
+          </div>
+        );
+    }
 }
 
 export default App;
