812dd968f74e0bcee711073e5addf94e36e6aa5d
potchi <richardcorrea2306@gmail.com>
2021-07-15 22:49:19 +0200

    React router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Main from './components/MainComponent';
 import React from 'react';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-social/bootstrap-social.css';
+import {BrowserRouter} from 'react-router-dom';
 
 function App(){
     return (
-      <div>        
-          <Main/>
-      </div>
+    
+        <BrowserRouter>
+            <div>        
+                <Main/>
+            </div>
+        </BrowserRouter>
     );
 }
 
