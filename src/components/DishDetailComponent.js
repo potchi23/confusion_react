95ac4b57647efa820bd8fbbb0ca23a9cd8ad6b46
potchi <richardcorrea2306@gmail.com>
2021-07-16 21:29:09 +0200

    Single Page Application Part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-import DateFormat from '../utils/DateFormat'
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import DateFormat from '../utils/DateFormat';
+import { Link } from 'react-router-dom';
 
 function renderComments(comments){
 
@@ -44,18 +45,34 @@ function renderDish(dish){
 function DishDetail(props) {
 
     const dish = props.dish;
+    const comments = props.comments;
 
     if(dish != null){
         return (
             <div className="container">
+
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+
+                    <div className="col-12">
+                        <h3>{dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+
                 <div className="row">
                     <div className="col-sm col-md-5 m-1">
                         {renderDish(dish)}
                     </div>
                     <div className="col-sm col-md-5 m-1">
-                        {renderComments(dish.comments)}
+                        {renderComments(comments)}
                     </div>
                 </div>
+                
             </div>
         );
     }
