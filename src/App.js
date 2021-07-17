454b3c084136928bc74b4342ac0d68a753404b5e
potchi <richardcorrea2306@gmail.com>
2021-07-17 23:17:17 +0200

    Redux

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Main from './components/MainComponent';
 import React from 'react';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-social/bootstrap-social.css';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore'; 
+
+const store = ConfigureStore();
 
 function App(){
+
     return (
-    
-        <BrowserRouter>
-            <div>        
-                <Main/>
-            </div>
-        </BrowserRouter>
+        <Provider store={store}>        
+            <BrowserRouter>
+                <div>        
+                    <Main/>
+                </div>
+            </BrowserRouter>
+        </Provider>
     );
 }
 
